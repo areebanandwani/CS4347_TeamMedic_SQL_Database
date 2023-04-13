ab1f3c4041d2047198a011e317c5cdd26cb2c685
Josh O <95489402+Vmoon1620@users.noreply.github.com>
2023-04-12 23:34:31 -0500

    Update and rename TeamMedic.DG.Workspace.sql to TeamMedicDGWorkspace.sql

---
+++ TeamMedicDGWorkspace.sql
@@ -0,0 +1,25 @@
+/* (Josh will do this one) 
+Institution (1-600) 
+    Hospital (1-200) 
+    Clinic (200-400) 
+    Pharmacy (400-600) 
+*/
+
+/*
+CREATE TABLE Institution(
+    IKey INT NOT NULL AUTO_INCREMENT,
+    AddressOfInstitution VARCHAR(80), /* changed from Address to AddressOfInstitution */
+    VitalCheckUp VARCHAR(80),
+    NameOfInstitution VARCHAR(80), 
+    PRIMARY Key (IKey)
+);
+
+Goal is to populate 600 enties for institution such that it can have enough data to 
+satistfy its class latice
+
+I will be using Mockaroo for this portion
+
+*/
+
+
+
